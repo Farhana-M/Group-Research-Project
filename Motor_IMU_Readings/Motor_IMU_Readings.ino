be25759a9bea1979027e833dced1f9de9b97714a
Takrim <131816829+TeaTax16@users.noreply.github.com>
2024-02-27 16:15:55 +0000

    Syringe Pump Integration
    IMU and Dynamixel XL330 were tested together successfully.
    Syringe pump running on XC430 was provided. Added position control capability. Maximum Velocity Control pending.

--- Motor_IMU_Readings/Motor_IMU_Readings.ino
+++ Motor_IMU_Readings/Motor_IMU_Readings.ino
@@ -1,5 +1,4 @@
 #include <Encoder.h>
-
 #include <Dynamixel2Arduino.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -9,12 +8,12 @@
 const uint8_t DXL_DIR_PIN = 2;
 Dynamixel2Arduino dxl(Serial1, DXL_DIR_PIN);
 const uint8_t M1 = 1;  // Motor 1
-const uint8_t M2 = 2;  // Motor 2
 const float res = 4096.0;  // Resolution of the motor
 const float degreesPerStep = 360.0 / res;  // Degrees per step
 
+
 // BNO055 setup
-Adafruit_BNO055 bno = Adafruit_BNO055(55)
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup() {
   Serial.begin(115200);
@@ -30,17 +29,14 @@ void setup() {
     Serial.println("Failed to connect to M1.");
     while (1); // Stop if connection fails
   }
-  // Test connection with M2
-  if (dxl.ping(M2)) {
-    Serial.println("M2 is connected!");
-  } else {
-    Serial.println("Failed to connect to M2.");
-    while (1); // Stop if connection fails
-  }
+  dxl.torqueOff(M1);
+  dxl.setOperatingMode(M1, OP_EXTENDED_POSITION);
+  dxl.torqueOn(M1);
+  
 
   //BNO055 Initialisation
   if (!bno.begin()){
-    Serial.println("No BNO055 detected.")
+    Serial.println("No BNO055 detected.");
     while (1); // Stop if connection fails
   }
   delay(1000); // Delay loop to complete setup
@@ -48,16 +44,14 @@ void setup() {
 
 void loop() {
   // Dynamixel Position and Velocities
-  // Get the position values for each motor and convert raw -> degrees
+  // Edit to change desired position
+  const float desPosM1 = -5000;
+  // Move motor to desired position
+  dxl.setGoalPosition(M1, desPosM1);
+  // Get the position values for each motor
   int32_t rawPos1 = dxl.getPresentPosition(M1);
-  float degPos1 = rawPos1 * degreesPerStep;
-  int32_t rawPos2 = dxl.getPresentPosition(M2);
-  float degPos2 = rawPos2 * degreesPerStep;
-  // Get the velocity values for each motor and convert raw -> degrees
+  // Get the velocity values for each motor
   int32_t rawVel1 = dxl.getPresentVelocity(M1);
-  float degVel1 = rawVel1 * degreesPerStep;
-  int32_t rawVel2 = dxl.getPresentVelocity(M2);
-  float degVel2 = rawVel2 * degreesPerStep;
 
   // BNO055 Orientation
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
@@ -65,14 +59,9 @@ void loop() {
   // Output all readings to Serial Monitor
   // M1 Outputs
   Serial.print("M1 || Position : ");
-  Serial.print(degPos1);
+  Serial.print(rawPos1);
   Serial.print(" | Velocity: ");
-  Serial.println(degVel1);
-  // M2 Outputs
-  Serial.print("M2 || Position : ");
-  Serial.print(degPos2);
-  Serial.print(" | Velocity: ");
-  Serial.println(degVel2);
+  Serial.println(rawVel1);
   // BNO055 Outputs
   Serial.print("IMU || Yaw(X): ");
   Serial.print(euler.x());
